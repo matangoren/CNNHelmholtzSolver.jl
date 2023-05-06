143cce271044826a93848ccce9d408cf202cc4c2
Matan Goren <matantheman@gmail.com>
2023-05-07 01:14:30 +0300

    up and down export

--- src/CNNHelmholtzSolver.jl
+++ src/CNNHelmholtzSolver.jl
@@ -1,7 +1,9 @@
 module CNNHelmholtzSolver
 
 include("../test/test_intro.jl")
+include("flux_components.jl")
 include("./solvers/cnn_helmholtz_solver.jl")
 
+export up, down
 export UNetUpBlock, FFSDNUnet, FFKappa, TFFKappa, TSResidualBlockI, SResidualBlock, FeaturesUNet 
 end
