a4e801a819e9915d86f7db2c15ed3ee0e6ba5554
Matan Goren <matantheman@gmail.com>
2023-05-06 23:46:36 +0300

    gg

--- src/CNNHelmholtzSolver.jl
+++ src/CNNHelmholtzSolver.jl
@@ -3,5 +3,5 @@ module CNNHelmholtzSolver
 include("../test/test_intro.jl")
 include("./solvers/cnn_helmholtz_solver.jl")
 
-# export UNetUpBlock, FFSDNUnet, FFKappa, TFFKappa, TSResidualBlockI, SResidualBlock, FeaturesUNet 
+export UNetUpBlock, FFSDNUnet, FFKappa, TFFKappa, TSResidualBlockI, SResidualBlock, FeaturesUNet 
 end
