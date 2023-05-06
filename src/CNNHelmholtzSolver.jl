86b1d27aec6c83d1c0a172810b35e0930b687b4b
Matan Goren <matantheman@gmail.com>
2023-05-06 21:53:44 +0300

    relative path

--- src/CNNHelmholtzSolver.jl
+++ src/CNNHelmholtzSolver.jl
@@ -2,14 +2,13 @@ module CNNHelmholtzSolver
 
 include("../test/test_intro.jl")
 
-include("unet/model.jl")
+include("./unet/model.jl")
 include("../test/test_utils.jl")
-include("gpu_krylov.jl")
-include("multigrid/helmholtz_methods.jl")
-include("data.jl")
-
-include("solvers/solver_utils.jl")
-include("solvers/cnn_helmholtz_solver.jl")
+include("./gpu_krylov.jl")
+include("./multigrid/helmholtz_methods.jl")
+include("./data.jl")
+include("./solvers/solver_utils.jl")
+include("./solvers/cnn_helmholtz_solver.jl")
 
 export UNetUpBlock, FFSDNUnet, FFKappa, TFFKappa, TSResidualBlockI, SResidualBlock, FeaturesUNet 
 end
